a1463a8897a079243815615105230b0fbe27a948
Blake Wilson <bwilson025@gmail.com>
2018-02-17 17:57:27 -0800

    Add script to draw simplex values

---
+++ draw_simplex.py
@@ -0,0 +1,43 @@
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+
+def draw_simplex(points):
+	trace = go.Heatmap(z=points)
+	data=[trace]
+	py.iplot(data, filename='basic-heatmap')
+
+def read_simplex(filepath):
+	"""read_simplex reads values of the simplex from the given
+	filepath. Returns the values of the simplex in the format
+	x11,x12,...,x1n,z1
+	x21,x22,...,x2n,z2
+	...
+	xn1,xn2,...,x(n+1)n, zn+1
+	where xij is the jth coordinate of the ith point and zi
+	is the evaulation of the ith point
+	"""
+	f = open(filepath, 'r')
+	points = []
+	while f.readline() != '':
+		for line in f:
+			print(line in set(['', 'Simplex\n']))
+			if line in ['', 'Simplex\n']:
+				break
+		# Read the points of the simplex
+		for line in f:
+			if line == 'End\n':
+				break
+			vals = line.split(',')
+			nums = [float(val) for val in vals]
+			print('nums are {}'.format(nums))
+			points.append(nums)
+
+	return points
+
+def main():
+	points = read_simplex('simplex.txt')
+	print(points)
+	draw_simplex(points)
+
+main()
